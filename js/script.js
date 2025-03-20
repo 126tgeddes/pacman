5c6f7caf17b363293ab007eedd7d2e503385bbc9
Timothy <126tgeddes@frhsd.com>
2025-03-20 12:15:28 -0400

    3/20/25 pacman collision

--- js/script.js
+++ js/script.js
@@ -11,15 +11,19 @@ window.onresize = function () {
 class Boundary {
     static width = 40
     static height = 40
-    constructor({ position }) {
+    constructor({ position, image }) {
         this.position = position
         this.width = 40
         this.height = 40
+        this.image = image
     }
 
     draw() {
-        c.fillStyle = 'blue'
-        c.fillRect(this.position.x, this.position.y, this.width, this.height)
+       // c.fillStyle = 'blue'
+       // c.fillRect(this.position.x, this.position.y, this.width, this.height)
+
+
+       c.drawImage(this.image, this.position.x, this.position.y)
     }
 }
 
@@ -72,7 +76,7 @@ const keys = {
 }
 
 let lastKey = ''
-//code is lukes
+
 
 const map = [
     ['-', '-', '-', '-', '-', '-', '-'],
